29345114a2f0ee126f6c1b71a3271e7a4922889c
DavideGB1 <davicolosimo23@gmail.com>
2024-10-16 12:33:44 +0200

    Unified PAOH

--- hypergraphx/viz/draw_PAOH.py
+++ hypergraphx/viz/draw_PAOH.py
@@ -7,7 +7,7 @@ from hypergraphx.viz.__support import __check_edge_intersection
 
 
 def draw_PAOH(
-    h: Hypergraph,
+    h: Hypergraph | TemporalHypergraph,
     space_optimization: bool = False,
     figsize: tuple[float, float] = (10, 10),
     dpi: int = 300,
@@ -21,6 +21,9 @@ def draw_PAOH(
     marker_edge_width: int = 3,
     y_label: str = "Nodes",
     x_label: str = "Edges",
+    time_font_size: int = 18,
+    time_separation_line_color: str = "#000000",
+    time_separation_line_width: int = 4,
     **kwargs) -> None:
     """
     Draws a PAOH representation of the hypergraph.
@@ -28,100 +31,6 @@ def draw_PAOH(
     ----------
     h : Hypergraph.
         The hypergraph to be projected.
-    space_optimization: bool
-        Flag used to determine if the column compression function should be used or not.
-    figsize : tuple, optional
-        Tuple of float used to specify the image size. Used only if ax is None.
-    dpi : int, optional
-        The dpi for the figsize. Used only if ax is None.
-    ax : plt.Axes, optional
-         Axis if the user wants to specify an image.
-    node_shape : str, optional
-        The shape of the nodes in the image. Use standard MathPlotLib values.
-    node_color : str, optional
-        HEX value for the nodes color.
-    node_size : int, optional
-        The size of the nodes in the image.
-    edge_color : str, optional
-        HEX value for the edges color.edge_width: float = 2
-    edge_width : float, optional
-        Width of the edges in the grid.
-    marker_edge_color : str, optional
-        Color of the node markers in each hyperedge.
-    marker_edge_width : int, optional
-        Width of the node markers in each hyperedge.
-    y_label : str, optional
-        Label for the y-axis.
-    x_label : str, optional
-        Label for the x-axis.
-    kwargs : dict.
-        Keyword arguments to be passed to the various MathPlotLib function.
-    Returns
-    -------
-    """
-    nodes_mapping = h.get_mapping()
-    #Get the edged position
-    if space_optimization:
-        column_list = __PAOH_edge_placemente_calculation(h.get_edges())
-    else:
-        column_list = []
-        for edge in h.get_edges():
-            column_list.append([edge])
-    if ax is None:
-        plt.figure(figsize=figsize, dpi=dpi)
-        plt.subplot(1, 1, 1)
-        ax = plt.gca()
-
-    idx = 0
-    #Draw each column
-    for column_set in column_list:
-        column_set = sorted(column_set)
-        #Plo each column in the relative position
-        for edge in column_set:
-            edge = tuple(sorted(edge))
-            first_node = edge[0]
-            last_node = edge[len(edge) - 1]
-            ax.plot([idx, idx], [nodes_mapping.transform([first_node])[0], nodes_mapping.transform([last_node])[0]],
-                     color=edge_color, linewidth = edge_width, **kwargs)
-            for y in edge:
-                ax.plot(idx, nodes_mapping.transform([y])[0], node_shape, color=node_color,
-                         markeredgecolor=marker_edge_color, markersize=node_size, markeredgewidth=marker_edge_width, **kwargs)
-        idx += 0.5
-
-    #Initiatiate the grid for the axes
-    ax.set_ylabel(y_label)
-    ax.set_xlabel(x_label)
-    ax.set_xticks([])
-    ax.set_xlim([-0.5, idx])
-    ax.set_yticks([y - 0.5 for y in range(0, h.num_nodes())], minor=True)
-    ax.grid(which="minor", ls="--", lw=1)
-
-
-def draw_temporal_PAOH(
-        h: TemporalHypergraph,
-        space_optimization: bool = False,
-        figsize: tuple[float, float] = (10, 10),
-        dpi: int = 300,
-        ax: Optional[plt.Axes] = None,
-        node_shape: str = "o",
-        node_color: str = "#FFFFFF",
-        node_size: int = 10,
-        edge_color: str = "#000000",
-        edge_width: float = 2,
-        marker_edge_color: str = "#000000",
-        marker_edge_width: int = 3,
-        y_label: str = "Nodes",
-        x_label: str = "Edges",
-        time_font_size: int = 18,
-        time_separation_line_color: str = "#000000",
-        time_separation_line_width: int = 4,
-        **kwargs) -> None:
-    """
-    Draws a PAOH representation of the temporal hypergraph.
-    Parameters
-    ----------
-    h : TemporalHypergraph.
-        The temporal hypergraph to be projected.
     space_optimization: bool
         Flag used to determine if the column compression function should be used or not.
     figsize : tuple, optional
@@ -164,63 +73,75 @@ def draw_temporal_PAOH(
         plt.subplot(1, 1, 1)
         ax = plt.gca()
 
-    edges = h.get_edges()
-    timestamp = dict()
-    for edge in edges:
-        if edge[0] not in timestamp:
-            timestamp[edge[0]] = []
-        timestamp[edge[0]].append(edge[1])
-
-    new_timestamps = list()
-
-    if space_optimization:
-        for edge_set in timestamp.values():
-            new_timestamps.append(__PAOH_edge_placemente_calculation(edge_set))
-    else:
-        for edge_set in timestamp.values():
-            new_list = []
-            for edge in edge_set:
-                new_list.append([edge])
-            new_timestamps.append(new_list)
-
     node_mapping = dict()
     idx = 0
     for node in h.get_nodes():
         node_mapping[idx] = node
         idx += 1
     max_node = idx - 0.5
+    timestamp_mapping = dict()
+    timestamps = []
+    if isinstance(h, TemporalHypergraph):
+        for edge in h.get_edges():
+            if edge[0] not in timestamp_mapping:
+                timestamp_mapping[edge[0]] = []
+            timestamp_mapping[edge[0]].append(edge[1])
+        if space_optimization:
+            for edge_set in timestamp_mapping.values():
+                timestamps.append(__PAOH_edge_placemente_calculation(edge_set))
+        else:
+            for edge_set in timestamp_mapping.values():
+                new_list = []
+                for edge in edge_set:
+                    new_list.append([edge])
+                timestamps.append(new_list)
+    else:
+        #Get the edged position
+        if space_optimization:
+            column_list = __PAOH_edge_placemente_calculation(h.get_edges())
+        else:
+            column_list = []
+            for edge in h.get_edges():
+                column_list.append([edge])
+        timestamps.append(column_list)
     idx = 0
     idx_timestamp = 0
-    for timestampy in new_timestamps:
-        ax.text(idx - 0.45, max_node - 0.175, "Time: " + str(list(timestamp.keys())[idx_timestamp]), fontsize=time_font_size, **kwargs)
-        for column_set in timestampy:
+    #Draw each column
+    for timestamp in timestamps:
+        if isinstance(h, TemporalHypergraph):
+            ax.text(idx - 0.45, max_node - 0.175, "Time: " + str(list(timestamp_mapping.keys())[idx_timestamp]),
+                    fontsize=time_font_size, **kwargs)
+        for column_set in timestamp:
             column_set = sorted(column_set)
+            #Plo each column in the relative position
             for edge in column_set:
                 edge = tuple(sorted(edge))
                 first_node = edge[0]
                 last_node = edge[len(edge) - 1]
-                ax.plot([idx, idx],
-                        [list(node_mapping.values()).index(first_node), list(node_mapping.values()).index(last_node)],
-                        color=edge_color, linewidth=edge_width, **kwargs)
+                ax.plot([idx, idx], [list(node_mapping.values()).index(first_node), list(node_mapping.values()).index(last_node)],
+                     color=edge_color, linewidth = edge_width, **kwargs)
                 for y in edge:
                     ax.plot(idx, list(node_mapping.values()).index(y), node_shape, color=node_color,
-                            markeredgecolor=marker_edge_color, markersize=node_size, markeredgewidth=marker_edge_width,
-                            **kwargs)
+                         markeredgecolor=marker_edge_color, markersize=node_size, markeredgewidth=marker_edge_width, **kwargs)
             idx += 0.5
-        ax.plot([idx, idx], [-0.5, max_node], color=time_separation_line_color, linewidth = time_separation_line_width, **kwargs)
-        idx_timestamp += 1
-        idx += 0.5
+        if isinstance(h, TemporalHypergraph):
+            ax.plot([idx, idx], [-0.5, max_node], color=time_separation_line_color,
+                    linewidth=time_separation_line_width, **kwargs)
+            idx_timestamp += 1
+            idx += 0.5
 
-    if not space_optimization:
+    if isinstance(h, TemporalHypergraph):
         idx -= 0.5
 
+    #Initiatiate the grid for the axes
     ax.set_ylabel(y_label)
     ax.set_xlabel(x_label)
     ax.set_xticks([])
     ax.set_xlim([-0.5, idx])
-    ax.set_yticks([y - 0.5 for y in range(0, 5)], minor=True)
+    ax.set_yticks(range(len(h.get_nodes())))
+    ax.set_yticklabels(h.get_nodes())
     ax.grid(which="minor", ls="--", lw=1)
-    ax.set_ylim([-0.5, max_node])
+
 
 def __PAOH_edge_placemente_calculation(l: list)  -> list:
     """
