3ecd254c10f5e20bd8a38271b4743a15502d5de8
DavideGB1 <davicolosimo23@gmail.com>
2025-03-15 22:20:33 +0100

    PAOH now fully support all the community detection algorithms

--- hypergraphx/viz/draw_PAOH.py
+++ hypergraphx/viz/draw_PAOH.py
@@ -1,12 +1,14 @@
 from typing import Optional
 
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import patches
 
 from hypergraphx import Hypergraph, DirectedHypergraph
 from hypergraphx.core.temporal_hypergraph import TemporalHypergraph
 from hypergraphx.viz.__graphic_options import GraphicOptions
 from hypergraphx.viz.__support import __ignore_unused_args, __filter_hypergraph, __check_edge_intersection, \
-    __support_to_normal_hypergraph, _get_community_info, _get_node_community
+    __support_to_normal_hypergraph, _get_community_info, _get_node_community, _draw_node_community
 from typing import Optional
 
 import matplotlib.pyplot as plt
@@ -160,25 +162,28 @@ def draw_PAOH(
                     for node in true_edge[0]:
                         ax.plot(idx, list(node_mapping.values()).index(node), marker=graphicOptions.node_shape[node],
                                 color=in_edge_color, markeredgecolor=graphicOptions.node_facecolor[node],
-                                markersize=graphicOptions.node_size[node]/30, **kwargs)
+                                markersize=graphicOptions.node_size[node]/20, **kwargs)
                     for node in true_edge[1]:
                         ax.plot(idx, list(node_mapping.values()).index(node), marker=graphicOptions.node_shape[node],
                                 color=out_edge_color, markeredgecolor=graphicOptions.node_facecolor[node],
-                                markersize=graphicOptions.node_size[node]/30, **kwargs)
+                                markersize=graphicOptions.node_size[node]/20, **kwargs)
 
                 else:
                     for node in edge:
                         if u is None:
                             ax.plot(idx, list(node_mapping.values()).index(node), marker= graphicOptions.node_shape[node],
                             color=graphicOptions.node_color[node], markeredgecolor=graphicOptions.node_facecolor[node],
-                            markersize=graphicOptions.node_size[node]/30, **kwargs)
+                            markersize=graphicOptions.node_size[node]/20, **kwargs)
                         else:
                             wedge_sizes, wedge_colors = _get_node_community(mapping, node, u, col, 0.1)
-                            ax.plot(idx, list(node_mapping.values()).index(node),
+                            if len(wedge_sizes) > 1:
+                                draw_pie_marker(ax,idx,list(node_mapping.values()).index(node),wedge_sizes, wedge_colors, marker_size = graphicOptions.node_size[node]/20,markeredgecolor=graphicOptions.node_facecolor[node] )
+                            else:
+                                ax.plot(idx, list(node_mapping.values()).index(node),
                                     marker=graphicOptions.node_shape[node],
                                     color=wedge_colors[0],
                                     markeredgecolor=graphicOptions.node_facecolor[node],
-                                    markersize=graphicOptions.node_size[node]/30 , **kwargs)
+                                    markersize=graphicOptions.node_size[node]/20 , **kwargs)
 
             idx += 0.5
         #Plot the separating line for the timestamps
@@ -245,3 +250,21 @@ def __PAOH_edge_placemente_calculation(l: list)  -> list:
         good_column_set = True
 
     return column_list
+
+def draw_pie_marker(ax, x, y, ratios, colors, marker_size=20, markeredgecolor='None'):
+    if len(ratios) != len(colors):
+        raise ValueError("Number of ratios and colors must be equal.")
+
+    cumulative_ratio = 0
+    for i, ratio in enumerate(ratios):
+        start_angle = cumulative_ratio
+        end_angle = cumulative_ratio + ratio
+
+        x_vals = np.cos(2 * np.pi * np.linspace(start_angle, end_angle))
+        y_vals = np.sin(2 * np.pi * np.linspace(start_angle, end_angle))
+        xy = np.row_stack([[0, 0], np.column_stack([x_vals, y_vals])])
+
+        ax.plot(x, y, marker=xy, ms=marker_size, markerfacecolor=colors[i],
+                markeredgecolor=markeredgecolor, linestyle='None')
+
+        cumulative_ratio = end_angle
